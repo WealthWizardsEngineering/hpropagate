c5dbe11d2a1e3233f8f0874db2f41c2999fe9e93
Pierre Meunier <pierre.meunier@wealthwizards.com>
2018-10-11 16:07:56 +0100

    chore: re-organised src

--- index.js
+++ index.js
@@ -1,84 +1,8 @@
 'use strict'
 
-const http = require('http')
-const uuid = require('uuid')
-const tracer = require('./tracer')
+const tracer = require('./lib/tracer')
+const http_wrapper = require('./lib/http_wrapper')
 
-const originalHttpCreateServer = http.createServer
-const originalRequest = http.request
+http_wrapper.wrapHttp()
 
-function setAndPropagateCorrelationId(req, res) {
-    let correlationId = req.headers['x-correlation-id']
-    if (typeof correlationId === 'undefined') {
-        correlationId = uuid.v4()
-    }
-    tracer.currentTrace.context.set('x-correlation-id', correlationId)
-    res.setHeader('x-correlation-id', correlationId)
-}
-
-function propagate(req, ...headers) {
-    headers.forEach(header => {
-        if (typeof req.headers[header] !== 'undefined') {
-            tracer.currentTrace.context.set(header, req.headers[header])
-        }
-    })
-}
-
-function inject(options, ...headers) {
-    console.log("DEBUG injecting headers for call to ", options.url)
-    headers.forEach(header => {
-        if (tracer.currentTrace.context.has(header)) {
-            console.log("DEBUG injecting ", header, " with ", tracer.currentTrace.context.get(header))
-            options.headers[header] = tracer.currentTrace.context.get(header)
-        }
-    })
-}
-
-function wrappedListener(listener) {
-
-    return (req, res, next) => {
-
-        tracer.newTrace('httpRequest')
-
-        setAndPropagateCorrelationId(req, res)
-
-        propagate(req,
-            'x-request-id',
-            'x-b3-traceid',
-            'x-b3-spanid',
-            'x-b3-parentspanid',
-            'x-b3-sampled',
-            'x-b3-flags',
-            'x-ot-span-context',
-            'x-variant-id')
-
-        listener.call(null, req, res, next)
-    }
-}
-
-function wrappedHttpCreateServer(listener) { // args are ([options<Object>], [listener<Fn>]) Express only sends listener
-
-    return originalHttpCreateServer(wrappedListener(listener))
-}
-
-function wrappedHttpRequest(options, cb) {
-
-    inject(options,
-        'x-correlation-id',
-        'x-request-id',
-        'x-b3-traceid',
-        'x-b3-spanid',
-        'x-b3-parentspanid',
-        'x-b3-sampled',
-        'x-b3-flags',
-        'x-ot-span-context',
-        'x-variant-id'
-    )
-
-    return originalRequest.call(null, options, cb)
-}
-
-http['createServer__original'] = originalHttpCreateServer
-http['createServer'] = wrappedHttpCreateServer
-http['request__original'] = originalRequest
-http['request'] = wrappedHttpRequest
+tracer.enable()
